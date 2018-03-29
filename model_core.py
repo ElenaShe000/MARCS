95f70dc884adcd41ae3553ce9a9e4ba949a822b2
ElenaShe000 <37882238+ElenaShe000@users.noreply.github.com>
2018-03-29 10:51:40 +0300

    Add files via upload

---
+++ model_core.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python 
+#  Description: MARCS MAIN MODULE WITH BASIC PARAMETRIZATION
+#  Experiment: SINGLE CATCHMENT
+#  05.03.2018
+#  Current Code Owner: Elena Shevnina, Finnish Meteorological Institute
+#  ph. +358449185446
+#  e-mail:eshevnina@gmail.com
+
+import sys
+import math as ma
+
+# Input Variables:
+# WID - id of the watershed
+# M1_REF = a first statistical moment of annual runoff, [mm year-1]; reference period
+# M2_REF = a second statistical moment of annual runoff, [mm year-1]^2; reference period
+# M3_REF = a third statistical moment of annual runoff, [mm year-1]^3; reference period
+# NPRE_REF = a mean annual precipitation amount, [mm year-1]; reference period
+# NPRE_PROJ 0 = a mean annual precipitation, [mm year-1]; projected period
+
+
+# Output Variables:
+# M1_PROJ = a first statistical moment of annual runoff, [mm year-1]; projected period
+# M2_PROJ = a second statistical moment of annual runoff, [mm year-1]^2; projected period
+# M3_PROJ = a third statistical moment of annual runoff, [mm year-1]^3; projected period
+# CV_PROJ = a coefficient of variation of annual runoff, [mm year-1]^2; projected period
+# CS_PROJ = a coefficient of skewness of annual runoff, [mm year-1]^3; projected period
+# CV_REF = a coefficient of variation of annual runoff, [mm year-1]^2; reference period
+# CS_REF = a coefficient of skewness of annual runoff, [mm year-1]^3; reference period
+
+# Inner Variables:
+# a, b0, b1 = the parameters of the Pearson Equation (as denoted in Andreev et al., 2005); reference period
+# a_proj, b0_proj, b1_proj = the parameters of the Pearson Equation
+# (as denoted in Andreev et al., 2005); projected period
+# c, Gcn, Gn = the parameters of the Fokker-Plank-Kolmogorov Equation
+# (as denoted in Kovalenko et al., 2006); reference period
+# c_proj, Gcn_proj, Gn_proj = the parameters of the Fokker-Plank-Kolmogorov Equation (Shevnina and Silaev, 2018);
+# projected period
+
+# Temporal Variables:
+
+# WORKING DIRECTORY
+WD = './'
+
+# MODEL INPUT: RUNOFF STATISTICAL MOMENTS, Reference climatology
+M1_REF = 379.230447067
+M2_REF = 149343.387513
+M3_REF = 60811610.4455
+NPRE_REF = 625.49223301
+
+# CATCHMENT ID
+WID = '6854600'
+
+
+# REFERENCE PERIOD: 1911-2014
+# AND PROJECTED PERIOD: 2020-2050
+
+
+# Convert string argument to digit
+def to_digit(x):
+    try:
+        if x.isdigit():
+            return int(x)
+        return float(x)
+    except ValueError:
+        return False
+
+
+# PARAMETERIZATION: GENERAL SCHEME, CONSTANT PARAMETERS
+# Equations 23-25 (Shevnina and Silaev, 2018) and (Kovalenko et al., 2006 p. 247-249)
+
+def basic_param_pearson(m1, m2, m3):
+    low_index = 2 * (m2 - m1 ** 2)
+    a_local = (5 * m1 * m2 - 4 * m1 ** 3 - m3) / low_index
+    b0_local = (m2 * m1 ** 2 - 2 * m2 ** 2 + m1 * m3) / low_index
+    b1_local = (3 * m1 * m2 - 2 * m1 ** 3 - m3) / low_index
+    return a_local, b0_local, b1_local
+
+
+# Equations 26-28 (Shevnina and Silaev, 2018) and (Kovalenko et. al 2006).
+
+def basic_param_fpk(a_local, b0_local, b1_local, npre_local):
+    low_index = (a_local - b1_local / 2)
+    gcn = npre_local * b1_local / low_index
+    gn = -2 * npre_local * b0_local / low_index
+    c = npre_local / low_index
+    return c, gn, gcn
+
+
+def foo_0(gcn_p, c_p, npre_proj):
+    # see Eq. (22-24) in Shevnina and Silaev, 2018
+    a_p = (gcn_p + 2 * npre_proj) / (2 * c_p)
+    b0_p = - gn_proj / (2 * c_p)
+    b1_p = gcn_p / c_p
+    return a_p, b0_p, b1_p
+
+
+def foo_1(m1, m2, m3, a_p, b0_p, b1_p):
+    # see Eq. (25-27) in Shevnina and Silaev, 2018
+    m1_p = a_p - b1_p
+    m2_proj = a_p * m1_p - 2 * m1_p * b1_p - b0_p
+    m3_proj = m2_proj * a_p - 2 * m1_p * b0_p - 3 * m2_proj * b1_p
+
+    # Coefficient of variation (CV) based on the moments (Rogdestvensky, 1988)
+    cv_r = ma.sqrt(m2 - m1 ** 2) / m1
+    cv_p = ma.sqrt(m2_proj - m1_p ** 2) / m1_p
+
+    # Moment skweness coefficient (Kovalenko et al. 2006, p.249)
+    cs_r = (m3 - 3 * m2 * m1 + 2 * m1 ** 3) / (cv_r ** 3 * m1 ** 3)
+    cs_p = (m3_proj - 3 * m2_proj * m1_p + 2 * m1_p ** 3) / (cv_p ** 3 * m1_p ** 3)
+
+    return cs_r, cs_p, m1_p, cv_p, cv_r
+
+
+# MAIN CODE
+
+# MODEL INPUT: projected climatology
+# NPRE_PROJ = 670
+NPRE_PROJ = to_digit(sys.argv[1])
+# Name_sce = str(sys.argv[1])
+
+if not NPRE_PROJ:
+    print('Use command: python model_core.py NPRE_PROJ')
+    exit(1)
+
+# PREDICTION: GENERAL SCHEME, CONSTANT PARAMETERS
+
+a, b0, b1 = basic_param_pearson(M1_REF, M2_REF, M3_REF)
+
+c_proj, gn_proj, gcn_proj = basic_param_fpk(a, b0, b1, NPRE_REF)
+
+a_proj, b0_proj, b1_proj = foo_0(gcn_proj, c_proj, NPRE_PROJ)
+
+cs_ref, cs_proj, m1_proj, cv_proj, cv_ref = foo_1(M1_REF, M2_REF, M3_REF, a_proj, b0_proj, b1_proj)
+
+# OUTPUT: TEXT LINE: WID, M1_REF, NPRE_REF, CV_REF, CS_REF, c, Gn, Gcn, M1_PROJ, NPRE_PROJ, CV_PROJ, CS_PROJ
+
+with open(WD + 'model_GPSCH.txt', 'a') as f:
+    f.write("%s %s %s %s %s %s %s %s %s %s %s %s\n" % (
+        WID, M1_REF, NPRE_REF, cv_ref, cs_ref, c_proj, gn_proj, gcn_proj, m1_proj, NPRE_PROJ, cv_proj, cs_proj))
---
+++ timeseries_meteo.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+import sys
+from netCDF4 import Dataset, num2date, date2index
+from math import ceil, floor
+from numpy import where, array
+from os import unlink
+
+#
+# CONFIG
+#
+
+IN_DIR = './'
+OUT_DIR = './'
+
+# Description
+#    'fname':  input filename (without directory)
+#    'value': name of "variable" (inside netCDF container) which contains a needed information
+#        (air temperature, precipitation, etc)
+#    'fn_postfix': tail of output file (with extension)
+#    'coef': arithmetic  function, that will be applied to each value (you can use any of python arithmetical functions)
+#    'group_func': group function, that will be applied for each year (it must be a numpy.ndarray method)
+#    'lat_resolution': lat grids resolution
+#    'lat_zero':   means the lat's grid is started from
+#        (for example: = 0 if the first value of variables["lat"] is 0)
+#    'lon_resolution': lon grids resolution
+#    'lon_zero': means the lon's grid is started from
+#        (for example: = 0 if the first value of variables["lon"] is 0)
+
+IF = {}
+
+IF['TAS_HM'] = {
+    'fname': 'tas_Amon_HadGEM2-AO_historical_r1i1p1_186001-200512.nc',
+    'value': 'tas',
+    'fn_postfix': '_tas_mod.txt',
+    'coef': ' - 272.15',
+    'group_func': 'mean()',
+    'lat_resolution': 1.25,
+    'lat_zero': 0,
+    'lon_resolution': 1.875,
+    'lon_zero': 0
+}
+
+IF['TAS_PR'] = {
+    'fname': 'tas_Amon_HadGEM2-AO_rcp85_r1i1p1_200601-210012.nc',
+    'value': 'tas',
+    'fn_postfix': '_tas_mod.txt',
+    'coef': ' - 272.15',
+    'group_func': 'mean()',
+    'lat_resolution': 1.25,
+    'lat_zero': 0,
+    'lon_resolution': 1.185,
+    'lon_zero': 0
+}
+
+IF['TAS_HO'] = {
+    'fname': 'air.mon.mean.v401.nc',
+    'value': 'air',
+    'fn_postfix': '_tas_obs.txt',
+    'coef': '',
+    'group_func': 'mean()',
+    'lat_resolution': 0.5,
+    'lat_zero': 0.25,
+    'lon_resolution': 0.5,
+    'lon_zero': 0.25
+}
+
+IF['PRE_HM'] = {
+    'fname': 'pr_Amon_HadGEM2-AO_historical_r1i1p1_186001-200512.nc',
+    'value': 'pr',
+    'fn_postfix': '_pre_mod.txt',
+    'coef': ' * 2592000',
+    'group_func': 'sum()',
+    'lat_resolution': 1.25,
+    'lat_zero': 0,
+    'lon_resolution': 1.875,
+    'lon_zero': 0
+}
+
+IF['PRE_PR'] = {
+    'fname': 'pr_Amon_HadGEM2-AO_rcp85_r1i1p1_200601-210012.nc',
+    'value': 'pr',
+    'fn_postfix': '_pre_obs.txt',
+    'coef': ' * 2592000',
+    'group_func': 'sum()',
+    'lat_resolution': 1.25,
+    'lat_zero': 0,
+    'lon_resolution': 1.875,
+    'lon_zero': 0
+}
+
+IF['PRE_HO'] = {
+    'fname': 'precip.mon.total.v401.nc',
+    'value': 'precip',
+    'fn_postfix': '_pre_obs.txt',
+    'coef': ' * 10',
+    'group_func': 'sum()',
+    'lat_resolution': 0.5,
+    'lat_zero': 0.25,
+    'lon_resolution': 0.5,
+    'lon_zero': 0.25
+}
+
+
+#
+# END CONFIG
+#
+
+# functions
+def to_digit(x):
+    try:
+        if x.isdigit():
+            return int(x)
+        return float(x)
+    except ValueError:
+        return False
+
+
+# Finding  nearest grid square
+def find_nearest(coord, scale, zero):
+    sign = 1
+    if coord < 0:
+        sign = -1
+        coord = abs(coord)
+
+    node = ceil(coord / scale) if coord - floor(coord / scale) > scale / 2 else floor(coord / scale)
+    return (node * scale - zero) * sign
+
+
+# Working with data
+def get_data_by_year(t_elem, config, f_lat, f_lon):
+    yearsum = {}
+    lon = find_nearest(f_lon, config['lon_resolution'], config['lon_zero'])
+    lat = find_nearest(f_lat, config['lat_resolution'], config['lat_zero'])
+
+    idx_lat = where(t_elem.variables["lat"][:] == lat)[0][0]
+    idx_lon = where(t_elem.variables["lon"][:] == lon)[0][0]
+
+    calendar = t_elem.variables["time"].calendar if hasattr(t_elem.variables["time"], 'calendar') else 'standard'
+    dates = num2date(t_elem.variables["time"][:], t_elem.variables["time"].units, calendar=calendar)
+
+    for time in dates:
+        time_id = date2index(time, t_elem.variables["time"])
+        if time.year not in yearsum:
+            yearsum[time.year] = [t_elem.variables[config['value']][time_id][idx_lat][idx_lon]]
+        else:
+            yearsum[time.year].append(t_elem.variables[config['value']][time_id][idx_lat][idx_lon])
+
+    for tyear in yearsum:
+        yearsum[tyear] = array(yearsum[tyear])
+        yearsum[tyear] = eval("yearsum[tyear]." + config['group_func'])
+        yearsum[tyear] = eval("yearsum[tyear] " + config['coef'])
+    return yearsum, lon, lat
+
+
+# input parameters
+if len(sys.argv) < 4:
+    print('Use command: python timeseries_meteo.py LON LAT ID')
+    exit(1)
+
+LON = to_digit(sys.argv[1])
+LAT = to_digit(sys.argv[2])
+ID = to_digit(sys.argv[3])
+
+if not LON or not LAT or not ID:
+    print('Use command: python timeseries_meteo.py LON LAT ID')
+    exit(1)
+
+if LON > 360 or LAT > 90 or LAT < -90:
+    print('Wrong coordinates')
+    exit(1)
+
+# load all data from files
+rootgrp = {}
+
+for i in IF:
+    try:
+        rootgrp[i] = Dataset(IN_DIR + str(IF[i]['fname']), "r")
+    except OSError:
+        print("Can't open file: %s" % IN_DIR + str(IF[i]['fname']))
+        exit(1)
+
+# remove old files
+for i in IF:
+    try:
+        unlink('{}{}{}'.format(OUT_DIR, ID, IF[i]['fn_postfix']))
+    except OSError:
+        continue
+
+# create new files
+for i in rootgrp:
+    result, grid_lon, grid_lat = get_data_by_year(rootgrp[i], IF[i], LAT, LON)
+    if result:
+        with open('{}{}{}'.format(OUT_DIR, ID, IF[i]['fn_postfix']), 'a') as file:
+            for year in result:
+                file.write('%s %s %s %s \n' % (grid_lat, grid_lon, year, result[year]))
